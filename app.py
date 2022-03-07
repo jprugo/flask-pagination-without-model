784d1741bde87d2fe06e1c9a49f2a258b941382f
Juan Rueda <juan.rueda@neoris.com>
2022-03-07 15:53:59 -0500

    Capturing base url from request and environments passed by serverless.yml

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from handlers.routes import configure_routes
 
-from utils import get_connection_data
-
 app = Flask(__name__)
-conn_data = get_connection_data("secreto")
+
+#conn_data = get_connection_data("secreto")
 
 # dialect+driver://username:password@host:port/database
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://{user}:{password}@{host}:{port}/{database}"\
-    .format(*conn_data, **conn_data)
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://{user}:{password}@{host}:{port}/{database}"\
+#     .format(*conn_data, **conn_data)
 
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
 
-configure_routes(app, db)
+configure_routes(app, None) #db
 
 if __name__ == '__main__':
     app.run(debug=True)
